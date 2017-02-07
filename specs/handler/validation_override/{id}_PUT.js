72b02600e1d11b8c71b62c1110cf1532040435cd
Christian Hubinger <christian@trigo.at>
2017-02-07 17:11:50 +0100

    feature: property validation overrides for "query", "params" & "payload"
    closes #22

---
+++ specs/handler/validation_override/{id}_PUT.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Joi = require('joi');
+
+module.exports.config = {
+	validate: {
+		params: {
+			id: Joi.string(),
+		},
+		query: {
+			page: Joi.string(),
+		},
+		payload: {
+			name: Joi.string(),
+		},
+	},
+};
+
+module.exports.handler = (req, reply) =>
+	reply({
+		payload: req.payload,
+	});
+
